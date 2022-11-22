ffe6155f73d3c00e151520dbe9998c6afd13b70f
GonzaloPA23 <116847758+GonzaloPA23@users.noreply.github.com>
2022-11-22 15:01:17 -0500

    Create fortalezas.sql

---
+++ Relational Data Insertion/fortalezas.sql
@@ -0,0 +1,11 @@
+INSERT INTO fortalezas(id, descripcion)
+VALUES
+    (1001,'Creatividad'),
+    (1002,'Persistencia'),
+    (1003,'Liderazgo'),
+    (1004,'Estetica'),
+    (1005,'Concentracion'),
+    (1006,'Audacia'),
+    (1007,'Empatia'),
+    (1008,'Esperanza'),
+    (1009,'Adaptabilidad');
