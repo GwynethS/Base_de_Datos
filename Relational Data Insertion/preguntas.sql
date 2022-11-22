0805a5f55bd1524d388ed46028cf3cfd9e6a92f0
GonzaloPA23 <116847758+GonzaloPA23@users.noreply.github.com>
2022-11-22 15:05:14 -0500

    Create preguntas.sql

---
+++ Relational Data Insertion/preguntas.sql
@@ -0,0 +1,14 @@
+INSERT INTO preguntas(id, enunciado)
+VALUES
+    (11011,'¿Que te hace diferente a otros candidatos?'),
+    (11012,'¿Cual es tu mayor debilidad o defecto?'),
+    (11013,'¿Por que quieres trabajar en nuestra empresa?'),
+    (11014,'¿Que te gusta hacer en tu tiempo libre?'),
+    (11015,'¿Cual es el mayor reto y/o la mejor decision que has tomado en tu vida?'),
+    (11016,'¿Que podria contarme sobre su curriculum?'),
+    (11017,'¿Coincide el puesto con tus expectativas a futuro?'),
+    (11018,'¿Cual es su mayor logro?'),
+    (11019,'¿Que habilidades te ayudaran a tener exito en este puesto?'),
+    (11020,'¿Por que deberiamos contratarte?'),
+    (11021,'¿Que experiencias laborales y/o entrevistas ha tenido antes de la simulacion?'),
+    (11022,'¿Cuales son tus metas?');
