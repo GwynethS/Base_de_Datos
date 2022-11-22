2a78ad58f494209f5769cec7292f444cf6ca21f0
GonzaloPA23 <116847758+GonzaloPA23@users.noreply.github.com>
2022-11-22 14:59:36 -0500

    Create instituciones.sql

---
+++ Relational Data Insertion/instituciones.sql
@@ -0,0 +1,12 @@
+INSERT INTO instituciones(id, nombre, tipo)
+VALUES
+    (100011,'Universidad Complutense de Madrid','Universidad'),
+    (100012,'Universidad Nacional de Ingenieria','Universidad'),
+    (100013,'IDAT','Instituto'),
+    (100014,'Universidad Nacional Mayor de San Marcos','Universidad'),
+    (100015,'CIBERTEC','Instituto'),
+    (100016,'Universidad Peruana de Ciencias Aplicadas','Universidad'),
+    (100017,'Instituto Peruano de Marketing IPM','Instituto'),
+    (100018,'Universidad Nacional de Piura','Universidad'),
+    (100019,'IESTP Simon Bolivar','Instituto'),
+    (100020,'Universidad de Lima','Universidad');
