97f60a48e5d1947ca1149920440d4c635ba5ac69
GonzaloPA23 <116847758+GonzaloPA23@users.noreply.github.com>
2022-11-22 15:03:22 -0500

    Create idiomas.sql

---
+++ Relational Data Insertion/idiomas.sql
@@ -0,0 +1,17 @@
+INSERT INTO idiomas(id, nombre, nivel_expertiz)
+VALUES
+    (10101,'Ingles','Basico'),
+    (10102,'Ingles','Intermedio'),
+    (10103,'Ingles','Avanzado'),
+    (10104,'Español','Basico'),
+    (10105,'Español','Intermedio'),
+    (10106,'Español','Avanzado'),
+    (10107,'Portugues','Basico'),
+    (10108,'Portugues','Intermedio'),
+    (10109,'Portugues','Avanzado'),
+    (10110,'Frances','Basico'),
+    (10111,'Frances','Intermedio'),
+    (10112,'Frances','Avanzado'),
+    (10113,'Italiano','Basico'),
+    (10114,'Italiano','Intermedio'),
+    (10115,'Italiano','Avanzado');
