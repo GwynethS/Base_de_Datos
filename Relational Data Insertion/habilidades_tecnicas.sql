7505a934d7b92f5774f1aec9906173dd059057b7
GonzaloPA23 <116847758+GonzaloPA23@users.noreply.github.com>
2022-11-22 15:04:31 -0500

    Create habilidades_tecnicas.sql

---
+++ Relational Data Insertion/habilidades_tecnicas.sql
@@ -0,0 +1,9 @@
+INSERT INTO habilidades_tecnicas(id, descripcion, nivel_expertiz)
+VALUES
+    (1221,'Conocimiento de la Informatica',	'Avanzado'),
+    (1222,'Programacion','Avanzado'),
+    (1223,'Word,Excel y Power Point','Intermedio'),
+    (1224,'Conocimiento de Redes sociales','Avanzado'),
+    (1225,'Robotica','Intermedio'),
+    (1226,'Capacidad de redaccion','Avanzado'),
+    (1227,'Tecnicas de diseño grafico','Intermedio');
