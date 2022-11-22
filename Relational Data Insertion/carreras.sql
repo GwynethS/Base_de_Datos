298e2121b71360de6a89c7884858cb50b73fabdd
GonzaloPA23 <116847758+GonzaloPA23@users.noreply.github.com>
2022-11-22 14:58:54 -0500

    Create carreras.sql

---
+++ Relational Data Insertion/carreras.sql
@@ -0,0 +1,12 @@
+INSERT INTO carreras(id, nombre)
+VALUES
+    (1,'Ciencias Economicas y Empresariales'),
+    (2,'Ingenieria Industrial'),
+    (3,'Administracion de Empresas'),
+    (4,'Contabilidad'),
+    (5,'Computacion e Informatica'),
+    (6,'Ingenieria de sistemas'),
+    (7,'Negocios Internacionales'),
+    (8,'Farmacologia'),
+    (9,'Tecnico en Electronica Industrial'),
+    (10,'Derecho');
