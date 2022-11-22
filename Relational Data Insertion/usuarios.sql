890091b5e8f0f978611b0838d30571bd3404064e
GonzaloPA23 <116847758+GonzaloPA23@users.noreply.github.com>
2022-11-22 14:57:06 -0500

    Create usuarios.sql

---
+++ Relational Data Insertion/usuarios.sql
@@ -0,0 +1,12 @@
+INSERT INTO usuarios (id,nombre,apellido,edad,fecha_nacimiento,correo,num_celular)
+VALUES
+    (10001,'Hugo','Quispe', 20,'2002-02-10','hugo.quispe20@gmail.com',903405934),
+    (10002,'Martin','Flores', 22,'2000-10-10','martin.flores22@gmail.com',902439456),
+    (10003,'Lucas','Sanchez', 21,'2001-01-03','lucas.sanchez21@gmail.com',990234390),
+    (10004,'Mateo','Garcia', 30,'1992-10-24','mateo.garcia23@gmail.com',987234432),
+    (10005,'Leo','Rodriguez', 30,'1992-04-10','leo.rodriguez30@gmail.com',903045000),
+    (10006,'Miriam','Marin', 23,'1999-08-12','miriammarin@gmail.com',934954768),
+    (10007,'Julia','Cardenas', 25,'1997-06-16','juliacardenas@gmail.com',992293290),
+    (10008,'Gustavo','Valle', 28,'1994-11-10','gustavovalle@gmail.com',984256524),
+    (10009,'Margarita','Alcala', 27,'1995-01-13','margaritaalcala@gmail.com',998190966),
+    (10010,'Maria','Aranzazu', 22,'2000-08-19','mariaaranzazu@gmail.com',932064749);
