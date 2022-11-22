9b45779b067a5995385721104d1bd5bd7e50cb79
GonzaloPA23 <116847758+GonzaloPA23@users.noreply.github.com>
2022-11-22 15:04:55 -0500

    Create curriculums_habilidades_tecnicas.sql

---
+++ Relational Data Insertion/curriculums_habilidades_tecnicas.sql
@@ -0,0 +1,13 @@
+INSERT INTO curriculums_habilidades_tecnicas (curriculum_id, habilidad_tecnica_id)
+VALUES
+    (2011,1221),
+    (2012,1222),
+    (2013,1223),
+    (2014,1225),
+    (2015,1224),
+    (2016,1226),
+    (2017,1227),
+    (2018,1223),
+    (2018,1221),
+    (2018,1222),
+    (2019,1222);
